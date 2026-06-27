2ddbbe34a88a4870f98b66e76467ab8a1a761cec
Shreya pal <shreyapal7985@gmail.com>
2026-06-27 16:33:21 +0530

    add few comment related to safeareaprovider and safeareaview

--- App.tsx
+++ App.tsx
@@ -7,14 +7,17 @@
 
 import {  StyleSheet, View, Text } from 'react-native';
 import {
-  SafeAreaProvider
+  SafeAreaProvider,
+  SafeAreaView
 } from 'react-native-safe-area-context';
 
 
 function App() {
 return (
-    <SafeAreaProvider>
+    <SafeAreaProvider>//does not affect ui it's like an calculator which calculates inset (means empty spaces )
+      <SafeAreaView>// applies those insets to shift the content from the unsafe screen areas(means not overlap with status bar, notches)
             <Text>hello world</Text>
+            </SafeAreaView>
     </SafeAreaProvider>
   );
 }
