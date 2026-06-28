88f502c4ee0d52c81063325528889d3d328b3d40
Shreya pal <shreyapal7985@gmail.com>
2026-06-28 19:19:29 +0530

    copy methods of array and object

--- Javascript/Array/array_copy.js
+++ Javascript/Array/array_copy.js
@@ -1,8 +1,20 @@
-const student={
-    name:'sana',
-    age:43,
-    course:'mca'
-}
+/* copy methods of array --> slice(), [...arr], Array.from() 
+Deep Copy (Object/Array) -->	structuredClone()*/
 
+//first method(slice())
+const student=['shaili','shahsi']
 const property= [student.slice()]
 console.log(property)
+console.log(student)
+
+//second method ([...arr])
+const a=[1,2,3]
+const b=[...a];
+console.log(a)
+console.log(b)
+
+//third method (Array.from())
+ const dance=['kathak','bharatnatyam','hip hop']
+const dance1= Array.from(dance)
+console.log(dance)
+console.log(dance1)
--- Javascript/Object/object_copy.js
+++ Javascript/Object/object_copy.js
@@ -1,8 +1,22 @@
+/* copy method for obejcts -->   {...obj}, Object.assign() */
+
+
+//first method to copy(Object.assign())
 const student={
     name:'sana',
     age:43,
     course:'mca'
 }
-
-const property= {student.slice()}
+const property= Object.assign({},student)
 console.log(property)
+console.log(student)
+
+
+//second method to copy ({...obj}) 
+const hobby={
+    name1:'cricket',
+    name2:"football",
+
+}
+const hobbies=({...hobby})
+console.log(hobbies)
